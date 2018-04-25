13a09c0385860714fc29bed8cf391b54b33c3285
SimonTod <simon.todeschini@viacesi.fr>
2018-04-25 10:42:04 +0200

    Update SampleClassTest.java

--- src/test/java/SampleClassTest.java
+++ src/test/java/SampleClassTest.java
@@ -10,4 +10,18 @@ public class SampleClassTest
         SampleClass sample = new SampleClass();
         assertEquals("10 x 0 must equal 0", sample.multiply(10, 0), 0);
     }
+    
+    @Test
+    public void multiplyingByOneShouldReturnSame()
+    {
+        SampleClass sample = new SampleClass();
+        assertEquals("10 x 1 must equal 10", sample.multiply(10, 1), 10);
+    }
+    
+    @Test
+    public void multiplyingByOneShouldReturnEight()
+    {
+        SampleClass sample = new SampleClass();
+        assertEquals("10 x 1 must equal 8", sample.multiply(10, 1), 8);
+    }
 }
